478862770627a81a58ce12a32b099f489fc32f0c
Alain Smithers <152569647+realasmithers@users.noreply.github.com>
2024-03-20 15:44:50 -0700

    Update streamlit_app.py
    Changed streamlit.title back to st.title

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-streamlit.title("My Parents Healthy New Diner")
+st.title("My Parents Healthy New Diner")
 st.write("Choose the fruits you want in your Smoothie.")
 
 name_on_order = st.text_input('Name on Smoothie')
