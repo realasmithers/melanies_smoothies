38148a91d855a467f15953e8474ad34f187ff8fa
Alain Smithers <152569647+realasmithers@users.noreply.github.com>
2024-03-20 17:35:21 -0700

    Update streamlit_app.py
    Moved response to for loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -21,29 +22,20 @@ ingredient_list = st.multiselect(
 )
 
 if ingredient_list:
-    #st.write(ingredient_list)
-    #st.text(ingredient_list)
-
     ingredients_string = ''
     
     for fruit_chosen in ingredient_list:
         ingredients_string += fruit_chosen + ' '
-    
-    #st.write(ingredients_string)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_dataframe = st.dataframe(fruityvice_response.json(), use_container_width=True)
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                 values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
     
-    #st.write(my_insert_stmt)
-    #st.stop
-
     time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_dataframe = st.dataframe(fruityvice_response.json(), use_container_width=True)
+
